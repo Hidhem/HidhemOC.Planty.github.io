9d1326c78c96f5dfc8ebb0fc0dbca96387ca6add
Hidhem <hassaini.mehdi@hotmail.fr>
2026-01-13 14:40:26 +0100

    ajout du header personnalisé

--- themes/blankslate_Children/functions.php
+++ themes/blankslate_Children/functions.php
@@ -4,4 +4,17 @@ function theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
+
+function blankslate_support()
+{
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('menus');
+    register_nav_menu('header', 'En tête du menu');
+    register_nav_menu('footer', 'Pied de page');
+}
+
+add_action('after_setup_theme', 'blankslate_support');
+
 ?>
+
---
+++ themes/blankslate_Children/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width">
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+    <header class="header__nav">
+        <a href="<?php echo home_url('/'); ?>" class="nav-logo">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo.png" alt="Logo">
+        </a>
+
+        <?php wp_nav_menu([
+            'theme_location' => 'header', 
+            'container' => false,
+            'menu_class' => 'navbar-nav',
+            ]) 
+        ?>
+    </header>
